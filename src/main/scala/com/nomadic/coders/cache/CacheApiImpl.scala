aa596450ae013a529e21450bbc29095f83d3cb6e
Josiah Ebhomenye <josiah.ebhomenye@news.co.uk>
2017-11-18 16:55:48 +0000

    added dispatcher configuration

--- src/main/scala/com/nomadic/coders/cache/CacheApiImpl.scala
+++ src/main/scala/com/nomadic/coders/cache/CacheApiImpl.scala
@@ -19,9 +19,9 @@ import scala.reflect.ClassTag
 private[cache] class CacheApiImpl(config: Config) extends CacheApi{
   import CacheManager._
   implicit val timeout = Timeout(5 seconds)
-   private val (system, cacheMgr) = {
-     val sys = ActorSystem("async-cache", config)
-     (sys, sys.actorOf(Props[CacheManager]))
+   private val cacheMgr ={
+     ActorSystem("async-cache", config)
+       .actorOf(Props(classOf[CacheManager], config.getInt("cache-size"), timeout), "cacheMgr")
    }
 
   def get[T](key: String)(implicit tag: ClassTag[T]): Future[Option[T]] = {
@@ -47,15 +47,15 @@ private[cache] class CacheApiImpl(config: Config) extends CacheApi{
 
 import com.nomadic.coders.cache.CacheManager._
 
-private[cache] class CacheManager extends Actor with ActorLogging{
+private[cache] class CacheManager(size: Int)(implicit val timeout: Timeout) extends Actor with ActorLogging{
   import context._
-  implicit val timeout = Timeout(5 seconds)
+
   val stored = new Stored
-  val watcher = context.actorOf(Props(classOf[EntityWatcher], self))
+  val watcher = context.actorOf(Props(classOf[EntityWatcher], self), "watcher")
 
   val router: Router = {
     Router(ConsistentHashingRoutingLogic(system)
-      , Vector.fill(5)(ActorRefRoutee(context.actorOf(Props(classOf[Cache], self, watcher)))))
+      , Vector.fill(size)(ActorRefRoutee(context.actorOf(Props(classOf[Cache], self, watcher, timeout)))))
   }
 
   def receive: Receive = {
@@ -71,15 +71,14 @@ private[cache] class CacheManager extends Actor with ActorLogging{
     case add @ Add(key, value, expire) =>  router.route( add, sender())
     case remove @ Remove(key) => stored.remove(key); router.route(remove, sender())
     case Clear => stored.clear(); router.route(Broadcast(Clear), sender())
-    case p: Persisted => stored.add(p); sender ! Done
+    case p: Key => stored.add(p); sender ! Done
     case msg @ RemoveExpired(p) if stored(p) => stored.remove(p); router.route(msg, sender())
   }
 }
 
-private [cache] class Cache(cacheMgr: ActorRef, watcher: ActorRef) extends Actor{
+private [cache] class Cache(cacheMgr: ActorRef, watcher: ActorRef)(implicit val timeout: Timeout) extends Actor{
 
   import context._
-  implicit val timeout = Timeout(5 seconds)
   private val store = mutable.Map[String, Entity]()
 
   def receive: Receive = {
@@ -88,9 +87,9 @@ private [cache] class Cache(cacheMgr: ActorRef, watcher: ActorRef) extends Actor
       val entity = Entity(value, LocalDateTime.now(), expire)
       store.put(key, entity)
       watcher ! (key, entity)
-      cacheMgr ? Persisted(key, entity.timestamp) pipeTo sender()
+      cacheMgr ? Key(key, entity.timestamp) pipeTo sender()
     case Remove(key) => store.remove(key); sender() ! Done
-    case RemoveExpired(Persisted(key, timestamp)) =>
+    case RemoveExpired(Key(key, timestamp)) =>
       store.get(key).foreach( entity => if(entity.timestamp == timestamp) store.remove(key) )
     case Clear => store.clear(); sender() ! Done
   }
@@ -102,7 +101,7 @@ private [cache] class EntityWatcher(cacheMgr: ActorRef) extends Actor{
   def receive: Receive = {
     case (key: String, Entity(_, timestamp, expiry)) if expiry != Duration.Inf =>
       // FIXME scheduler not optimal in high throughput situations, use HashedWheelTimer instead
-      system.scheduler.scheduleOnce(expiry){ cacheMgr ! RemoveExpired(Persisted(key, timestamp))}
+      system.scheduler.scheduleOnce(expiry){ cacheMgr ! RemoveExpired(Key(key, timestamp))}
   }
 }
 
@@ -117,12 +116,12 @@ private [cache] object CacheManager{
   case class Get(key: String) extends Hashable
   case class GetOrElse(key: String, value: Any, expire: Duration) extends Hashable
   case class Remove(key: String) extends Hashable
-  case class RemoveExpired(persisted: Persisted) extends Hashable{
-    override def key: String = persisted.key
+  case class RemoveExpired(persistedKey: Key) extends Hashable{
+    override def key: String = persistedKey.value
   }
 
   case class Entity(value: Any, timestamp: LocalDateTime, expiry: Duration)
-  case class Persisted(key: String, timestamp: LocalDateTime)
+  case class Key(value: String, timestamp: LocalDateTime)
   case object Clear
 
   val Done = ()
@@ -131,17 +130,17 @@ private [cache] object CacheManager{
 }
 
 class Stored{
-  private val map = mutable.Map[String, Persisted]()
+  private val map = mutable.Map[String, Key]()
 
-  def add(persisted: Persisted) = map.put(persisted.key, persisted)
+  def add(persisted: Key) = map.put(persisted.value, persisted)
 
   def apply(key: String): Boolean = map.contains(key)
 
-  def apply(persisted: Persisted): Boolean ={
-    map.get(persisted.key).exists(_.timestamp == persisted.timestamp)
+  def apply(persisted: Key): Boolean ={
+    map.get(persisted.value).exists(_.timestamp == persisted.timestamp)
   }
 
-  def remove(persisted: Persisted): Unit = remove(persisted.key)
+  def remove(persisted: Key): Unit = remove(persisted.value)
 
   def remove(key: String): Unit = map.remove(key)
 
